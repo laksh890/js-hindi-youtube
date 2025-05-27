0863438fa264d6732f452cbf15e338ecbc65e1ee
laksh890 <156300293+laksh890@users.noreply.github.com>
2025-05-27 16:08:48 +0000

    memory concept

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -50,3 +50,27 @@ console.log(typeof bigNumber)
 */
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3 
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive),  Heap (Non-Primitive)
+let myYoutubename = "LakshayGandotracom"
+let anothername = myYoutubename 
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+// Heap memory reference 
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "lakshay@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
